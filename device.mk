9a5ae1f7e0252c74c9052025458be71e84e43f17
klozz <shaclone707@gmail.com>
2019-01-21 05:12:17 -0500

    vince: Remove RRO targets i will fix them soon
    Signed-off-by: klozz <shaclone707@gmail.com>

--- device.mk
+++ device.mk
@@ -20,9 +20,6 @@ $(call inherit-product, vendor/xiaomi2/vince/vince-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Enforce RRO
-PRODUCT_ENFORCE_RRO_TARGETS := framework-res SystemUI SettingsProvider Settings Bluetooth
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
